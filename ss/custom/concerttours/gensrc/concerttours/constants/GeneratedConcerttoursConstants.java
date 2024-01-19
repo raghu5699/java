74ee707377cf8a95d175632f981f5744cc112227
raghu5699 <2023.raghavendra.hybris@gmail.com>
2024-01-19 15:49:16 +0530

    ss
    ss

---
+++ ss/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 19 Jan, 2024 3:07:01 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ ss/custom/concerttours/web/webroot/index.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>concerttours</h1>
+		Welcome to my extension.
+	</body>
+</html>
+
